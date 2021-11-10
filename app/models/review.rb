cacc5544f788d1fe01ec16f1242b927c011cb9fe
David Mambou <david.mambou@gmail.com>
2021-11-10 15:19:10 +0900

    As usual little to no style effort but hey, it works

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,10 @@ class Review < ApplicationRecord
   belongs_to :restaurant
   validates :restaurant, presence: true
   validates :content, presence: true
-  validates :rating, presence: true, numericality: { only_integer: true, less_than: 6, greater_than_or_equal_to: 0 }
+  validates :rating,
+            presence: true,
+            numericality: {
+              only_integer: true, less_than_or_equal_to: 5, greater_than_or_equal_to: 0,
+              message: 'must be an integer between 0 and 5'
+            }
 end
